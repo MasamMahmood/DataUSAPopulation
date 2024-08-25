1e19bf158b75b1c2038f9eab50a9123eb0c22032
Masam Mahmood <masammahmood@hotmail.com>
2024-08-25 19:11:27 +0100

    Initial Commit

---
+++ DataUSAPopulation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DataUSAPopulation
+//
+//  Created by Masam Mahmood on 25/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DataUSAPopulation/DataUSAPopulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataUSAPopulationApp.swift
+//  DataUSAPopulation
+//
+//  Created by Masam Mahmood on 25/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct DataUSAPopulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
